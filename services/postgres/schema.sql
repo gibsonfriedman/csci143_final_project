d795f7197ad54eb0e75a240fd204d31e6743ae6d
gibsonfriedman <gibsonafriedman@gmail.com>
2024-05-04 15:23:04 -0700

    changes

--- services/postgres/schema.sql
+++ services/postgres/schema.sql
@@ -1,3 +1,6 @@
+SET max_parallel_maintenance_workers TO 80;
+SET maintenance_work_mem TO '16 GB';
+
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
@@ -17,3 +20,13 @@ create table messages (
     created_at timestamp not null default current_timestamp,
     id_urls INTEGER REFERENCES urls(id_urls)
 );
+
+CREATE TABLE fts_word(
+	word text PRIMARY KEY
+);
+
+CREATE EXTENSION IF NOT EXISTS RUM;
+CRATE EXTESNION IF NOT EXISTS pg_trgm;
+
+CREATE INDEX retrieve_messages ON messages(created_at, id, sender_id, message);
+CREATE INDEX query_messages ON messages USING RUM (to_tsvector('english', message));
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,55 +1,297 @@
 import os
-
+import sqlalchemy
+import bleach
+import datetime
 from flask import (
     Flask,
     jsonify,
     send_from_directory,
     request,
+    make_response,
+    redirect,
+    render_template
 )
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config.from_object("project.config.Config")
-db = SQLAlchemy(app)
+
+engine = sqlalchemy.create_engine("postgresql://postgres:pass@postgres:5432", connect_args={'application_name': '__init__.py', })
+connection = engine.connect()
 
 
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
+def make_cookie(username, password):
+    response = make_response(redirect('/'))
+    response.set_cookie('username', username)
+    response.set_password('password', password)
+    return response
 
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def debug_info():
+    print("request.args.get('username')=", request.args.get('username'))
+    print("request.args.get('password')=", request.args.get('password'))
+
+    print("request.form.get('username')=", request.form.get('username'))
+    print("request.form.get('password')=", request.form.get('password'))
+
+    print("request.cookies.get('username')=", request.cookies.get('username'))
+    print("request.cookies.get('password')=", request.cookies.get('password'))
 
 
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
+def credential_check(username, password):
+    sql = sqlalchemy.sql.text("""
+            SELECT id FROM users WHERE username = :username AND password = :password;""")
+    res = connection.execute(sql, {
+        'username': username,
+        'password': password})
+    if res.fetchone() is None:
+        return False
+    else:
+        return True
 
 
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+def get_messages(a):
+    messages = []
+    sql = sqlalchemy.sql.text("""
+        SELECT sender_id,
+        message,
+        created_at,
+        id
+        FROM messages ORDER BY created_at DESC LIMIT 40 OFFEST :offset;""")
+    res = connection.execute(sql, {'offset': a})
+
+    for row_messages in res.fetchall():
+        sql = sqlalchemy.sql.text("""
+                SELECT id,
+                username,
+                password,
+                age
+                FROM users WHERE id=:id;""")
+        user_res = connection.execute(sql, {'id': row_messages[0]})
+        row_users = user_res.fetchone()
+        message = row_messages[1]
+        cleaned_message = bleach.clean(message)
+        linked_message = bleach.linkify(cleaned_message)
+        messages.append({
+            'id': row_messages[3],
+            'message': linked_message,
+            'username': row_users[1],
+            'age': row_users[3],
+            'created_at': row_messages[2]})
+    return messages
 
 
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
+def query_messages(query, a):
+    sql = sqlalchemy.sql.text("""
+        SELECT sender_id,
+        ts_headline(
+            message, to_tsquery(:query),
+            'StartSel = "<span class = query><b>",
+            StopSel = "</b></span>"')
+        AS highlighted_message,
+        created_at,
+        messages.id,
+        username,
+        age
+        FROM messages JOIN users ON (messages.sender_id = users.id)
+        WHERE to_tsvector('english', message) @@ to_tsquery(:query)
+        ORDER BY to_tsvectory(message) <=> to_tsquery(:query),
+        created_at DESC LIMIT 20 OFFSET :offset;""")
+    print('inside query_messages')
+
+    res = connection.execute(sql, {
+        'offset': 40 * (a - 1),
+        'query': ' & '.join(query.split())
+    })
+
+    messages = []
+    for row_messages in res.fetchall():
+        message = row_messages[1]
+        cleaned_message = bleach.clean(message, tags=['b', 'span'], attributes={'span': ['class']})
+        linked_message = bleach.linkify(cleaned_message)
+        messages.append({
+            'id': row_messages[3],
+            'message': linked_message,
+            'username': row_messages[4],
+            'age': row_messages[5],
+            'created_at': row_messages[2]})
+    return messages
+
+
+@app.route('/')
+def root():
+    debug_info()
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_credentials = credential_check(username, password)
+    print('good_credentials', good_credentials)
+    page_number = int(request.args.get('page', 1))
+    messages = get_messages(page_number)
+    return render_template('root.html', messages=messages, good_credentials=good_credentials, username=username, page_number=page_number)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    debug_info()
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_credentials = credential_check(username, password)
+    print('good_credentials = ', good_credentials)
+
+    if good_credentials:
+        return redirect('/')
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    good_credentials = credential_check(username, password)
+    print('good credentials = ', good_credentials)
+
+    if username is None:
+        return render_template('login.html', bad_credentials=False)
+
+    else:
+        if not good_credentials:
+            return render_template('login.html', bad_credentials=True)
+        else:
+            response = make_response(redirect('/'))
+            response.set_cookie('username', username)
+            response.set_cookie('password', password)
+            return response
+
+
+@app.route('/logout')
+def logout():
+    debug_info()
+    response = make_response(render_template('logout.html'))
+    response.delete_cookie('username')
+    response.delete_cookie('password')
+    return response
+
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    debug_info()
+
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+
+    good_credentials = credential_check(username, password)
+    print("good_credentials = ", good_credentials)
+
+    if good_credentials:
+        return redirect('/')
+
+    username_new = request.form.get('username_new')
+    password_new = request.form.get('password_new')
+    password_new2 = request.form.get('password_new2')
+    age_new = request.form.get('new_age')
+
+    if username_new is None:
+        return render_template('create_user.html')
+
+    elif not username_new or not password_new:
+        return render_template('create_user.html', one_blank=True)
+
+    elif not age_new.isnumeric():
+        return render_template('create_user.html', invalid_age=True)
+
+    else:
+        if password_new != password_new2:
+            return render_template('create_user.html', password_mismatch=True)
+        else:
+            try:
+                sql = sqlalchemy.sql.test("""INSERT into users (username, password, age) values(:username, :password, :age);
+                        """)
+
+                res = connection.execute(sql, {
+                    'username': username_new,
+                    'password': password_new,
+                    'age': age_new})
+                print(res)
+                return make_cookie(username_new, password_new)
+            except sqlalchemy.exc.IntegrityError:
+                return render_template('create_user.html', already_exists=True)
+
+
+@app.route('/create_message', methods=['GET', 'POST'])
+def create_message():
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_credentials = credential_check(username, password)
+    if not good_credentials:
+        return redirect('/')
+
+    sql = sqlalchemy.sql.text('''SELECT id FROM users
+                                 WHERE username = :username AND password = :password''')
+
+    res = connection.execute(sql, {
+        'username': username,
+        'password': password})
+
+    for row in res.fetchall():
+        sender_id = row[0]
+
+    message = request.form.get('message')
+
+    if message is None:
+        return render_template('create_message.html', logged_in=good_credentials)
+    elif not message:
+        return render_template('create_message.html', invalid_message=True, logged_in=good_credentials)
+    else:
+        created_at = str(datetime.datetime.now()).split('.')[0]
+        sql = sqlalchemy.sql.text("""
+        INSERT INTO messages (sender_id,message,created_at) VALUES (:sender_id, :message, :created_at);
+        """)
+        res = connection.execute(sql, {
+            'sender_id': sender_id,
+            'message': message,
+            'created_at': created_at})
+    print(res)
+
+    return render_template('create_message.html', logged_in=good_credentials, message_posted=True)
+
+
+@app.route('/delete_message', methods=['GET', 'POST'])
+def delete_message():
+    message_id = request.form.get('message_id')
+
+    sql = sqlalchemy.sql.text("""
+        DELETE FROM messages WHERE id = :id;
+        """)
+
+    res = connection.execute(sql, {
+        'id': message_id})
+    print(res)
+    return redirect('/')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    debug_info()
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_credentials = credential_check(username, password)
+    if good_credentials:
+        logged_in = True
+    else:
+        logged_in = False
+    print('logged-in=', logged_in)
+
+    page_number = int(request.args.get('page', 1))
+
+    if request.form.get('query'):
+        query = request.form.get('query')
+    else:
+        query = request.args.get('query', '')
+
+    print('before query_messages')
+
+    if query:
+        messages = query_messages(query, page_number)
+    else:
+        messages = get_messages(page_number)
+
+    response = make_response(render_template('search.html', messages=messages, logged_in=logged_in, username=username, page_number=page_number, query=query))
+    if query:
+        response.set_cookie('query', query)
+
+    return response
